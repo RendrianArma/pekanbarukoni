5ccedbf6ab3fc248f6323f017086a5613347a120
Rendrian Arma <me@rendrian.id>
2018-10-05 07:20:53 +0700

    add login page

---
+++ page.login.php
@@ -0,0 +1,81 @@
+aasdas
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dashmix - Bootstrap 4 Admin Template &amp; UI Framework | DEMO</title>
+    <meta name="description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest | This is the demo of Dashmix! You need to purchase a license for legal use! | DEMO">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="noindex, nofollow">
+    <meta property="og:title" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework | DEMO">
+    <meta property="og:site_name" content="Dashmix">
+    <meta property="og:description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest | This is the demo of Dashmix! You need to purchase a license for legal use! | DEMO">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
+    <link rel="stylesheet" id="css-main" href="assets/css/dashmix.min-1.4.css">
+
+</head>
+<body>
+	<div id="page-container">
+		<main id="main-container">
+			<div class="row no-gutters justify-content-center bg-body-dark">
+				<div class="hero-static col-sm-10 col-md-8 col-xl-6 d-flex align-items-center p-2 px-sm-0">
+					<div class="block block-rounded block-transparent block-fx-pop w-100 mb-0 overflow-hidden bg-image" style="background-image: url('assets/media/photos/photo5.jpg');">
+						<div class="row no-gutters">
+							<div class="col-md-6 order-md-1 bg-white">
+								<div class="block-content block-content-full px-lg-5 py-md-5 py-lg-6">
+									<div class="mb-2 text-center">
+										<a class="link-fx font-w700 font-size-h1" href="index.html">
+											<span class="text-dark">KONI</span><span class="text-primary">Pekanbaru</span>
+										</a>
+										<p class="text-uppercase font-w700 font-size-sm text-muted">Sign In</p>
+									</div>
+									<form class="js-validation-signin" action="be_pages_auth_all.html" method="post">
+										<div class="form-group">
+											<input type="text" class="form-control form-control-alt" id="login-username" name="login-username" placeholder="Username / Email">
+										</div>
+										<div class="form-group">
+											<input type="password" class="form-control form-control-alt" id="login-password" name="login-password" placeholder="Password">
+										</div>
+										<div class="form-group">
+											<button type="submit" class="btn btn-block btn-hero-primary">
+												<i class="fa fa-fw fa-sign-in-alt mr-1"></i> Sign In
+											</button>
+										</div>
+									</form>
+								</div>
+							</div>
+							<div class="col-md-6 order-md-0 bg-primary-dark-op d-flex align-items-center">
+								<div class="block-content block-content-full px-lg-5 py-md-5 py-lg-6">
+									<div class="media">
+										<a class="img-link mr-3" href="javascript:void(0)">
+											<img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar4.jpg" alt="">
+										</a>
+										<div class="media-body">
+											<p class="text-white font-w600 mb-1">
+												Selamat datang di sistem Koni Pekanbaru
+											</p>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+	<script src="assets/js/dashmix.core.min-1.4.js"></script>
+	<script src="assets/js/dashmix.app.min-1.4.js"></script>
+	<script src="assets/js/plugins/jquery-validation/jquery.validate.min.js"></script>
+	<script src="assets/js/pages/op_auth_signin.min.js"></script>
+</body>
+</html>
