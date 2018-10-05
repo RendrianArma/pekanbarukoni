e141732f053842fcf51d93a354fd55dc64fa70f4
Rendrian Arma <me@rendrian.id>
2018-10-05 11:26:06 +0700

    perbaiki

--- page_footer.php
+++ page_footer.php
@@ -1,49 +1,53 @@
 <footer id="page-footer" class="footer-static bg-white">
-  <div class="content py-4">
-    <div class="row items-push font-size-sm border-bottom pt-4">
-      <div class="col-6 col-md-4 text-center">
-        <h3 class="h5 font-w700">Link Terkait</h3>
-        <ul class="list list-simple-mini">
-          <li>
-            <a class="link-fx font-w600" href="javascript:void(0)">KoniRiau.org</a>
-          </li>
-          <li>
-            <a class="link-fx font-w600" href="javascript:void(0)">Riau.go.id</a>
-          </li>
+    <div class="content py-4">
+        <div class="row items-push font-size-sm border-bottom pt-4">
+            <div class="col-6 col-md-4 text-center">
+                <h3 class="h5 font-w700">Link Terkait</h3>
+                <ul class="list list-simple-mini">
+                    <li>
+                    </li>
+                    <li>
+                        <a class="link-fx font-w600" href="javascript:void(0)">Riau.go.id</a>
+                    </li>
 
-        </ul>
-      </div>
-      <div class="col-6 col-md-4 text-center">
-        <h3 class="h5 font-w700">Link Penting</h3>
-        <ul class="list list-simple-mini">
-          <li>
-            <a class="link-fx font-w600" href="javascript:void(0)">Link Penting</a>
-          </li>
-          <li>
-          </ul>
+                </ul>
+            </div>
+            <div class="col-6 col-md-4 text-center">
+                <h3 class="h5 font-w700">Link Penting</h3>
+                <ul class="list list-simple-mini">
+                    <li>
+                        <a class="link-fx font-w600" href="javascript:void(0)">Link Penting</a>
+                    </li>
+                    <li>
+                </ul>
+            </div>
+            <div class="col-md-4 text-center">
+                <h3 class="h5 font-w700">KONTAK</h3>
+                <div>
+                    Jl. Arifin Ahmad, Tengkerang Tengah, Marpoyan Damai,
+                    <br>
+                    Kota Pekanbaru, Riau 28125<br>
+                    <abbr title="Phone">P:</abbr>
+                    (0761) 456-7890
+                </div>
+            </div>
         </div>
-        <div class="col-md-4 text-center">
-          <h3 class="h5 font-w700">KONTAK</h3>
-          <div>
-            Jl. Arifin Ahmad, Tengkerang Tengah, Marpoyan Damai, <br>
-            Kota Pekanbaru, Riau 28125<br>
-            <abbr title="Phone">P:</abbr> (0761) 456-7890
-          </div>
+        <div class="row font-size-sm pt-4">
+            <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-right">
+                Designed By
+                <i class="fa fa-heart text-danger"></i>
+                by
+                <a class="font-w600" href="https://rendrian.id" target="_blank">Rendrian Arma</a>
+            </div>
+            <div class="col-sm-6 order-sm-1 text-center text-sm-left">
+                <a class="font-w600" href="https://goo.gl/mDBqx1" target="_blank">KoniPekanbaru</a>
+                ©
+                <span data-toggle="year-copy" class="js-year-copy-enabled">2018</span>
+            </div>
         </div>
-      </div>
-      <div class="row font-size-sm pt-4">
-        <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-right">
-          Designed By <i class="fa fa-heart text-danger"></i> by <a class="font-w600" href="https://rendrian.id" target="_blank">Rendrian Arma</a>
-        </div>
-        <div class="col-sm-6 order-sm-1 text-center text-sm-left">
-          <a class="font-w600" href="https://goo.gl/mDBqx1" target="_blank">KoniPekanbaru</a> © <span data-toggle="year-copy" class="js-year-copy-enabled">2018</span>
-        </div>
-      </div>
     </div>
-  </footer>
+</footer>
 </div>
-
-
 
 <script src="assets/js/dashmix.core.min-1.4.js"></script>
 <script src="assets/js/dashmix.app.min-1.4.js"></script>
@@ -57,7 +61,6 @@
 <script src="assets/js/datatables/dataTables.jszip.min.js"></script>
 <script src="assets/js/datatables/be_tables_datatables.min.js"></script>
 
-
-
 </body>
+
 </html>
